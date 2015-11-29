8f7cc3fee6f2b32f6bdcf21f7a1a0454bad566b8
Randall Sexton <jrandallsexton@gmail.com>
2015-11-29 09:23:51 -0600

    completed Part 1 of the tutorial

--- AspNetIdentity/AspNetIdentity.WebApi/Infrastructure/ApplicationUserManager.cs
+++ AspNetIdentity/AspNetIdentity.WebApi/Infrastructure/ApplicationUserManager.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
 
 namespace AspNetIdentity.WebApi.Infrastructure
 {
-    class ApplicationUserManager
+    public class ApplicationUserManager : UserManager<ApplicationUser>
     {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+        {
+            var appDbContext = context.Get<ApplicationDbContext>();
+            var appUserManager = new ApplicationUserManager(new UserStore<ApplicationUser>(appDbContext));
+
+            return appUserManager;
+        }
     }
 }
